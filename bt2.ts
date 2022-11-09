7620ac65a40993cb2edb2e7c3ae495352d1965bc
Tân Phù <26tiamo12@gmail.com>
2022-11-09 14:19:03 +0700

    bt2

---
+++ bt2.ts
@@ -0,0 +1,8 @@
+console.log('BT2: Nhập 2 số thực x,y và tính hàm')
+var BT2 = function(x: number, y: number) {
+    let tu: number = Math.log(Math.abs(x*x - y*y))/ Math.log(5) + Math.atan(x+y)
+    let mau: number = Math.exp(x) + Math.cos(x+y)
+    let F: number = Math.round((tu/mau)*100)/100
+    console.log('Gia tri la cua ham =',F)
+}
+BT2(2,3)
