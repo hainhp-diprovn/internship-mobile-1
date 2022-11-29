5e17560d7b899961ec0fec8b6a34cc182a5b1273
Tân Phù <26tiamo12@gmail.com>
2022-11-29 13:40:18 +0700

    (fix): style

--- internship/src/screens/TanPCM/BaiTap2/BaiTap2.tsx
+++ internship/src/screens/TanPCM/BaiTap2/BaiTap2.tsx
@@ -15,53 +15,53 @@ import {avatars, img2, img3} from '../url';
 const  BaiTap2 = () => {
   const data = [
     {
-      title: 'Number 1',
-      name: 'Devin',
+      title: "Number 1",
+      name: "Devin",
       image: avatars,
     },
     {
-      title: 'Number 2',
-      name: 'Dan',
+      title: "Number 2",
+      name: "Dan",
       image: img2,
     },
     {
-      title: 'Number 3',
-      name: 'Dominic',
+      title: "Number 3",
+      name: "Dominic",
       image: img3,
     },
     {
-      title: 'Number 4',
-      name: 'Jackson',
+      title: "Number 4",
+      name: "Jackson",
       image: avatars,
     },
     {
-      title: 'Number 5',
-      name: 'James',
+      title: "Number 5",
+      name: "James",
       image: img3,
     },
     {
-      title: 'Number 6',
-      name: 'Joel',
+      title: "Number 6",
+      name: "Joel",
       image: img2,
     },
     {
-      title: 'Number 7',
-      name: 'John',
+      title: "Number 7",
+      name: "John",
       image: avatars,
     },
     {
-      title: 'Number 8',
-      name: 'Jillian',
+      title: "Number 8",
+      name: "Jillian",
       image: img2,
     },
     {
-      title: 'Number 9',
-      name: 'Jimmy',
+      title: "Number 9",
+      name: "Jimmy",
       image: img3,
     },
     {
-      title: 'Number 10',
-      name: 'Julie',
+      title: "Number 10",
+      name: "Julie",
       image: avatars,
     },
   ];
@@ -88,8 +88,8 @@ export default BaiTap2;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f8ff',
-    alignItems: 'center',
+    backgroundColor: "#f0f8ff",
+    alignItems: "center",
   },
   listItem: {
     padding: 10,
@@ -102,6 +102,6 @@ const styles = StyleSheet.create({
     backgroundColor: "blue",
     textAlign: "center",
     fontSize: 20,
-    width: 100,
+    paddingVertical: 10
   },
 });
