c6f5a1b195d398fc4891084bc5fe1fdec55dc305
Tân Phù <26tiamo12@gmail.com>
2022-11-29 11:23:51 +0700

    (fix): style2

--- internship/src/screens/TanPCM/BaiTap1/BaiTap1.tsx
+++ internship/src/screens/TanPCM/BaiTap1/BaiTap1.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {StyleSheet,Image,Text, SafeAreaView} from 'react-native'
+import {StyleSheet,Image,Text, SafeAreaView, View} from 'react-native'
 import { avatars } from '../url';
 
 const BaiTap1 = () => {
     return (
           <SafeAreaView style={styles.container}>
+            <View style={styles.avatar}>
               <Image source={avatars} style={styles.image} />
               <Text style={styles.text}>Name</Text>
+            </View>
           </SafeAreaView>
     )
 }
@@ -15,17 +17,22 @@ export default BaiTap1
 const styles = StyleSheet.create({
     container: {
         flex:1,
+        flexDirection: "row",
         margin: 20,
       },
-      image: {
+      avatar: {
         height: 150,
         width: 110,
+        
+      },
+      image: {
+        width:"100%",
+        height: 150, 
       },
       text: {
         backgroundColor: "blue",
-        fontSize: 20,
         textAlign: "center",
-        width: 110,
-        height:30
+        fontSize: 20,
+        paddingVertical: 10
       },
 })
