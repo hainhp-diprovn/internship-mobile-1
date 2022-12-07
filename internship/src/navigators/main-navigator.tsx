a41ceed4723a527df40f8daa344eb79c59490a4a
DPT1004 <phuthanhdo104@gmail.com>
2022-12-07 14:11:55 +0700

    @thanhdp/feat/layout/baitap4

--- internship/src/navigators/main-navigator.tsx
+++ internship/src/navigators/main-navigator.tsx
@@ -21,6 +21,7 @@ import BT7DPT from "../screens/ThanhDP/screens/View/BT7"
 import SliderVertical from '../screens/ThanhDP/screens/View/SliderVertical';
 import SliderHorizontal from '../screens/ThanhDP/screens/View/SliderHorizontal';
 import BT1_LayoutDPT from "../screens/ThanhDP/screens/Layout/BT1"
+import BT4_LayoutDPT from "../screens/ThanhDP/screens/Layout/BT4"
 
 
 const Stack = createNativeStackNavigator()
