b8e093053190891e409d0e1806813afaf624e73b
DPT1004 <phuthanhdo104@gmail.com>
2022-12-07 14:24:55 +0700

    @thanhdp/feat/layout/baitap444

---
+++ internship/src/screens/ThanhDP/screens/Layout/BT444.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    Dimensions
+} from 'react-native';
+
+const BT444 = () => {
+
+    const [orientation, setOrientation] = useState("PORTRAIT");
+    const DEVICE_SCREEN_WIDTH = Dimensions.get('window').width;
+
+    useEffect(() => {
+        Dimensions.addEventListener('change', ({ window: { width, height } }) => {
+            if (width < height) {
+                setOrientation("PORTRAIT")
+            } else {
+                setOrientation("LANDSCAPE")
+            }
+        })
+    }, [orientation]);
+
+    return (
+        <View style={styles.container}>
+            <View style={[styles.square, { backgroundColor: 'white', width: 50 }]} />
+            <View style={[styles.square, { width: 80, height: 80 }]} />
+            <View style={[styles.square, { backgroundColor: 'white', flex: 1 }]} />
+            <View style={[styles.square, { width: 80, height: 80 }]} />
+            <View style={[styles.square, { backgroundColor: 'white', flex: 1 }]} />
+            <View style={[styles.square, { width: 80, height: 80 }]} />
+            <View style={[styles.square, { backgroundColor: 'white', width: 50 }]} />
+        </View>
+    )
+}
+
+export default BT444
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    square: {
+        backgroundColor: 'pink',
+    }
+});
+
