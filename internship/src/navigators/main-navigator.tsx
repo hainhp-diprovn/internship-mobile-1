581415106c89a8ef858eb04b183480e29d81acc4
DPT1004 <phuthanhdo104@gmail.com>
2022-12-07 16:19:01 +0700

    @thanhdp/feat/layout/baitap9

--- internship/src/navigators/main-navigator.tsx
+++ internship/src/navigators/main-navigator.tsx
@@ -36,6 +36,7 @@ const MainStack = () => {
     >
       <Stack.Screen name={screenName.home} component={HomeScreen} />
       <Stack.Screen name={screenName.profile} component={ProfileScreen} />
+
     </Stack.Navigator>
   )
 }
