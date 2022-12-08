ae7645b04ab7d2c5497c308052422d17bbf355ea
Tân Phù <26tiamo12@gmail.com>
2022-12-08 15:49:38 +0700

    (feat): bai tap 3

--- internship/src/navigators/main-navigator.tsx
+++ internship/src/navigators/main-navigator.tsx
@@ -19,7 +19,7 @@ import BT3DPT from "../screens/ThanhDP/screens/View/BT3"
 import BT5DPT from "../screens/ThanhDP/screens/View/BT5"
 import BT7DPT from "../screens/ThanhDP/screens/View/BT7"
 import BaiTap1_Layout from "../screens/TanPCM/layout/BaiTap1"
-// import BaiTap11_Layout from "../screens/TanPCM/layout/BaiTap1'"
+import BaiTap11_Layout from "../screens/TanPCM/layout/BaiTap1'"
 
 import SliderVertical from '../screens/ThanhDP/screens/View/SliderVertical';
 import SliderHorizontal from '../screens/ThanhDP/screens/View/SliderHorizontal';
