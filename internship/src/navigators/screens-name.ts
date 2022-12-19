5c74bd982bdcbda882a247322bd8a168ccaece3e
Tân Phù <26tiamo12@gmail.com>
2022-12-19 17:13:15 +0700

    (feat): bai tap 4

--- internship/src/navigators/screens-name.ts
+++ internship/src/navigators/screens-name.ts
@@ -8,4 +8,10 @@ export enum screenName {
     ScreenC_PCMT = "ScreenC_PCMT",
     ScreenD_PCMT = "ScreenD_PCMT",
     ScreenE_PCMT = "ScreenE_PCMT",
+
+    ScreenPage1_PCMT = "ScreenPage1_PCMT",
+    ScreenPage2_PCMT = "ScreenPage2_PCMT",
+    ScreenPage3_PCMT = "ScreenPage3_PCMT",
+    ScreenPage4_PCMT = "ScreenPage4_PCMT",
+    ScreenPage5_PCMT = "ScreenPage5_PCMT",
 }
